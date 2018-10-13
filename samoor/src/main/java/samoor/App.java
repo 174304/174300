2819c92b938c5476919e91dce658164d85623c23
unknown <iamnadeesh@gmail.com>
2018-10-13 23:42:15 +0530

    [foo]

--- samoor/src/main/java/samoor/App.java
+++ samoor/src/main/java/samoor/App.java
@@ -1,13 +1,28 @@
 package samoor;
 
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+   
+   
+    @Test
+    
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	int a =3;
+    	int b =4;
+    	int c = a+b;
+    	assertEquals(c, 7);
+    	System.out.println( "Hello World!" );
     }
+    
+    
 }
